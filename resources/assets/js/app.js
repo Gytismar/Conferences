ecf607b0e3a145d0fa4750decc3f70c246febe81
Gytismar <25962211+Gytismar@users.noreply.github.com>
2024-04-07 02:01:03 +0300

    Changing project configuration, adding some test coe

---
+++ resources/assets/js/app.js
@@ -0,0 +1,9 @@
+import './bootstrap';
+require('flatpickr')
+document.addEventListener('DOMContentLoaded',function (){
+  document.querySelectorAll('.datepicker').forEach(function (item){
+      flatpickr(item,{
+          mode:'range'
+      });
+  });
+});
