49acdf5edb5d47497ff5ac40a3f9c83f40836804
Gytismar <25962211+Gytismar@users.noreply.github.com>
2024-04-10 02:33:16 +0300

    Added Login and Logout logic to base view

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,9 +1,16 @@
 import './bootstrap';
 require('flatpickr')
-document.addEventListener('DOMContentLoaded',function (){
-  document.querySelectorAll('.datepicker').forEach(function (item){
-      flatpickr(item,{
-          mode:'range'
-      });
-  });
+// document.addEventListener('DOMContentLoaded',function (){
+//   document.querySelectorAll('.datepicker').forEach(function (item){
+//       flatpickr(item,{
+//           mode:'range'
+//       });
+//   });
+// });
+
+$(function (){
+    $(document).on('click', '#logout-btn', function(e){
+        e.preventDefault();
+        $('#logout-form').trigger('submit');
+    });
 });
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,9 +5,23 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
     <title>Laravel project - @yield('title')</title>
 </head>
 <body>
+<div>
+    <header style="margin: 10px">
+        @guest
+            <a href="{{ route('login') }}">Login</a>
+        @else
+            <a href="{{ route('logout') }}" id="logout-btn">Logout ({{ auth()->user()->name }})</a>
+            <form action="{{ route('logout') }}" method="POST" id="logout-form" class="d-none">
+                @csrf
+            </form>
+        @endguest
+    </header>
     @yield('content')
+</div>
+<script src="{{ mix('js/app.js') }}"></script>
 </body>
 </html>
