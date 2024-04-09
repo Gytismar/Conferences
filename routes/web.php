d50000b35b744e7c33abc37455bbecd364cfc2a2
Gytismar <25962211+Gytismar@users.noreply.github.com>
2024-04-10 01:40:51 +0300

    Added path to Auth

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use \App\Http\Controllers\Auth\LoginController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Carbon;
 use Illuminate\Http\Request;
