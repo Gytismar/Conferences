3f22a8537d88da73154a82ed5ea207185b4fc12e
Gytismar <25962211+Gytismar@users.noreply.github.com>
2024-04-10 03:33:22 +0300

    Added jquery

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,5 +1,7 @@
 import './bootstrap';
 require('flatpickr')
+import $ from 'jquery';
+window.$ = window.jQuery = $; //Accessible jquery via $
 // document.addEventListener('DOMContentLoaded',function (){
 //   document.querySelectorAll('.datepicker').forEach(function (item){
 //       flatpickr(item,{
