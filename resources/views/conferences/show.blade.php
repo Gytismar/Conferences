299df306690092d5c91c32fdbfeb3d0c01fd5a1d
Gytismar <25962211+Gytismar@users.noreply.github.com>
2024-04-10 18:20:15 +0300

    Reused list.blade.php in show.blade.php

--- resources/views/conferences/show.blade.php
+++ resources/views/conferences/show.blade.php
@@ -4,10 +4,5 @@
     @if(session('status'))
         <div style="background-color: green; color: lime">{{ session('status') }}</div>
     @endif
-    <h1>{{ $conference['title'] }}</h1>
-    <p>{{ $conference['content'] }}</p>
-    <p>Date: {{ \Carbon\Carbon::parse($conference['conference_date'])->format('F d, Y') }}</p>
-    <p>Address: {{ $conference['address'] }}</p>
-    <p>Attending People Count: {{ $conference['attending_people_count'] }}</p>
-    <a href="{{ route('conferences.edit', ['conference' => $conference['id']]) }}"><button type="button">Edit</button></a>
+    @include('conferences.partials.list', ['conference' => $conference])
 @endsection
