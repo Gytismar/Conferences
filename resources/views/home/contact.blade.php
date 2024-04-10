f5da75c3d63001faafd595308abedacadc0a574d
Gytismar <25962211+Gytismar@users.noreply.github.com>
2024-04-11 01:40:29 +0300

    Adding a bit of bootstrap to home and contact pages

--- resources/views/home/contact.blade.php
+++ resources/views/home/contact.blade.php
@@ -3,5 +3,11 @@
 @section('title', __('site.contact_page_title'))
 
 @section('content')
-    <h1>@lang('site.contact_page_title')</h1>
+    <div class="container mt-5">
+        <div class="row">
+            <div class="col text-center">
+                <h1 class="display-4">@lang('site.contact_page_title')</h1>
+            </div>
+        </div>
+    </div>
 @endsection
--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -3,5 +3,11 @@
 @section('title', __('site.homepage_title'))
 
 @section('content')
-    <h1>@lang('site.homepage_title')</h1>
+    <div class="container mt-5">
+        <div class="row">
+            <div class="col text-center">
+                <h1 class="display-4">@lang('site.homepage_title')</h1>
+            </div>
+        </div>
+    </div>
 @endsection
